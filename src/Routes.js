0592f083ceb5637c3c79c5667218a50ca6371ed8
Mohammed Matin <mmatin@uncc.edu>
2020-12-15 07:54:50 -0500

    Login/logout feature added

--- src/Routes.js
+++ src/Routes.js
@@ -2,16 +2,20 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./page/Home";
 import NotFound from "./page/NotFound";
+import Login from "./page/Login";
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/">
         <Home />
       </Route>
+      <Route exact path="/login">
+        <Login />
+      </Route>
       {/* Finally, catch all unmatched routes */}
-    <Route>
-  <NotFound />
-</Route>
+      <Route>
+        <NotFound />
+      </Route>
     </Switch>
   );
 }
